e3fce9377716db4c733e2273fc0021b47733c675
arun2352000 <poorvaja10@gmail.com>
2024-10-13 09:58:59 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import './App.css';
+import Navbar from './components/homepage/Navbar';
 const App = () => {
   return (
     <div>
-      
+      <h1>app page</h1>
+      <Navbar />
     </div>
   );
 };
--- src/components/homepage/Navbar.jsx
+++ src/components/homepage/Navbar.jsx
@@ -1,44 +1,60 @@
 import React from 'react';
+// import { Link, useNavigate } from 'react-router-dom';
+// import Search from './Search';
+// import {useDispatch, useSelector} from 'react-redux';
+// import {DropdownButton, Dropdown, Image} from 'react-bootstrap';
+// import { logout } from '../../actions/userActions';
+
 
 const Navbar = () => {
+  // const { isAuthenticated, user } = useSelector(state => state.authState);
+  //   const { items:cartItems } = useSelector(state => state.cartState)
+  //   const dispatch = useDispatch();
+  //   const navigate = useNavigate();
+  //   const logoutHandler = () => {
+  //     dispatch(logout);
+    // }
     return (
         <div>
-            <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            {/* <li><hr class="dropdown-divider"></li> */}
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input> 
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
+             <nav className="navbar row">
+        <div className="col-12 col-md-3">
+          <div className="navbar-brand">
+            <Link to="/">
+              <img width="150px" alt='JVLcart Logo' src="/images/logo.png" />
+            </Link>
+            </div>
+        </div>
+  
+        <div className="col-12 col-md-6 mt-2 mt-md-0">
+           <Search/>
+        </div>
+  
+        <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
+          { isAuthenticated ? 
+            (
+              <Dropdown className='d-inline' >
+                  <Dropdown.Toggle variant='default text-white pr-5' id='dropdown-basic'>
+                    <figure className='avatar avatar-nav'>
+                      <Image width="50px" src={user.avatar??'./images/default_avatar.png'}  />
+                    </figure>
+                    <span>{user.name}</span>
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                      { user.role === 'admin' && <Dropdown.Item onClick={() => {navigate('admin/dashboard')}} className='text-dark'>Dashboard</Dropdown.Item> }
+                      <Dropdown.Item onClick={() => {navigate('/myprofile')}} className='text-dark'>Profile</Dropdown.Item>
+                      <Dropdown.Item onClick={() => {navigate('/orders')}} className='text-dark'>Orders</Dropdown.Item>
+                      <Dropdown.Item onClick={logoutHandler} className='text-danger'>Logout</Dropdown.Item>
+                  </Dropdown.Menu>
+              </Dropdown>
+            )
+          
+          :
+            <Link to="/login"  className="btn" id="login_btn">Login</Link>
+          }
+          <Link to="/cart"><span id="cart" className="ml-3">Cart</span></Link>
+          <span className="ml-1" id="cart_count">{cartItems.length}</span>
+        </div>
+    </nav>
         </div>
     );
 };
